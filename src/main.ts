eed04bc748fb03c696c71bd01a9e3c170cdb9f44
Guilherme Corsino <guilermecorsino22@gmail.com>
2026-07-26 18:15:08 -0300

    feat: add Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
 import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,        // remove campos que não estão no DTO
-    forbidNonWhitelisted: true, // rejeita se mandar campo extra
-    transform: true,        // converte tipos automaticamente
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
   }))
+
+  const config = new DocumentBuilder()
+    .setTitle('PromoVendas API')
+    .setDescription('API de gestão de estoque e vendas com multi-tenancy')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
 
   await app.listen(3000)
 }
